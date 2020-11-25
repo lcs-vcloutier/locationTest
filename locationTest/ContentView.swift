34f319a5f976c4d6539d4c975f05564dfd5cd925
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-25 10:13:11 -0500

    Initial Commit

---
+++ locationTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  locationTest
+//
+//  Created by Cloutier, Vincent on 2020-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ locationTest/locationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  locationTestApp.swift
+//  locationTest
+//
+//  Created by Cloutier, Vincent on 2020-11-25.
+//
+
+import SwiftUI
+
+@main
+struct locationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
