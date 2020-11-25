602bcae667b720a7be4a11ae155cc21632cce409
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-25 10:13:28 -0500

    added in code from https://www.hackingwithswift.com/100/swiftui/78 for testing

--- locationTest/ContentView.swift
+++ locationTest/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    let locationFetcher = LocationFetcher()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Start Tracking Location") {
+                self.locationFetcher.start()
+            }
+
+            Button("Read Location") {
+                if let location = self.locationFetcher.lastKnownLocation {
+                    print("Your location is \(location)")
+                } else {
+                    print("Your location is unknown")
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
     }
 }
